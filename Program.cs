88d61f5b79cb15e648205435fd303e74d51818ed
Afalex <34140310+Afalex@users.noreply.github.com>
2020-12-11 15:32:50 +0400

    add Disperion

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MultiplicativeModel
 {
@@ -6,16 +8,39 @@ namespace MultiplicativeModel
     {
         static void Main(string[] args)
         {
-            double x = 100;
-            double z = 100;
+            double x = 10;
+            double z = 10;
 
             double n = Math.Pow(x, 1 / z);
 
+            List<double> values = new List<double>();
+
+
             for (int i = 0; i < z; i++)
             {
-                Console.WriteLine(Math.Pow(n, i));
+                values.Add(Math.Pow(n, i));
             }
+
+
+
+            foreach (var val in values)
+                Console.WriteLine(val);
+
+
+            Console.WriteLine("Avverage: " + GetAvverage(values));
+            Console.WriteLine("Disperion: " + GetDisperion(values));
+            Console.WriteLine("Standard deviation: " + Math.Sqrt(GetDisperion(values)));
             Console.Read();
+        }
+
+        public static double GetAvverage(List<double> values)
+        {
+            return values.Sum() / values.Count;
+        }
+
+        public static double GetDisperion(List<double> values)
+        {
+            return GetAvverage(values.Select(v => Math.Pow(v, 2)).ToList()) - Math.Pow(GetAvverage(values), 2);
         }
     }
 }
