ceb89967ee91e50a6c8ecf060b83a42ce3b45a66
Afalex <34140310+Afalex@users.noreply.github.com>
2020-12-11 15:25:04 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplicativeModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 100;
+            double z = 100;
+
+            double n = Math.Pow(x, 1 / z);
+
+            for (int i = 0; i < z; i++)
+            {
+                Console.WriteLine(Math.Pow(n, i));
+            }
+            Console.Read();
+        }
+    }
+}
