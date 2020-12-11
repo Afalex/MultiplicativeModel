7eb557ec3db2e26a2a3af9866acbb8c21098c4e3
Afalex <34140310+Afalex@users.noreply.github.com>
2020-12-11 15:53:36 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,10 +8,12 @@ namespace MultiplicativeModel
     {
         static void Main(string[] args)
         {
-            double x = 10;
-            double z = 10;
+            double queueSize = 200;
 
-            double n = Math.Pow(x, 1 / z);
+            double dispersionRatio = 100;
+            double z = 100;
+
+            double n = Math.Pow(dispersionRatio, 1 / z);
 
             List<double> values = new List<double>();
 
@@ -22,15 +24,40 @@ namespace MultiplicativeModel
             }
 
 
+            double sum = values.Sum();
 
+            Console.WriteLine("ratio sum: " + sum);
+            double XInequation = queueSize / sum; //GetFactorRatio(
+
+
+
+            List<double> absolutCount = new List<double>();
+
+            foreach (double val in values)
+            {
+                absolutCount.Add(val * XInequation);
+            }
+
+            ShowResult(absolutCount);
+
+
+            Console.Read();
+        }
+
+        public static void ShowResult(List<double> values)
+        {
             foreach (var val in values)
                 Console.WriteLine(val);
-
 
             Console.WriteLine("Avverage: " + GetAvverage(values));
             Console.WriteLine("Disperion: " + GetDisperion(values));
             Console.WriteLine("Standard deviation: " + Math.Sqrt(GetDisperion(values)));
-            Console.Read();
+
+            Console.WriteLine();
+
+            double countSum = values.Sum();
+
+            Console.WriteLine("count sum: " + countSum);
         }
 
         public static double GetAvverage(List<double> values)
@@ -41,6 +68,27 @@ namespace MultiplicativeModel
         public static double GetDisperion(List<double> values)
         {
             return GetAvverage(values.Select(v => Math.Pow(v, 2)).ToList()) - Math.Pow(GetAvverage(values), 2);
+        }
+
+        /// <summary>
+        ///  В случае большой конкуренции базовый коэффициент настолько мал, что не представляет собой целой число, 
+        ///  а количество минимальной очереди равно сумме чисел арифметической прогрессии с шагом 1/advCount 
+        ///  от a1 до an, но при целом значении базового коэффициента, определяющего поправку
+        /// </summary>
+        /// <param name="factor">используй GetBaseFactor</param>
+        /// <param name="factorRatio">определяет число, на которое нужно умножить коэффициент, чтобы получить базовый коэффициент для
+        /// поправки</param>
+        /// <returns></returns>
+        public static (double val, int factorRatio) GetFactorRatio(double factor, int factorRatio = 0)
+        {
+            if (factor < 1)
+            {
+                factorRatio++;
+                factor *= 10;
+            }
+            else
+                return (factor, factorRatio);
+            return GetFactorRatio(factor, factorRatio);
         }
     }
 }
