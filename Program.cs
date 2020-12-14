9d3a8dccc3d75996a30883d27b7a9b3550975f04
Afalex <34140310+Afalex@users.noreply.github.com>
2020-12-14 21:28:40 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,9 @@ namespace MultiplicativeModel
     {
         static void Main(string[] args)
         {
-            double membersCount = 10;
-            double maxCountForMember = membersCount * 2.5; //максимальное кол-во повторов, также контроль дисперсии
+            double membersCount = 100;
+            const double DISPERSION_LEVEL = 2.5;
+            double maxCountForMember = membersCount * DISPERSION_LEVEL; //максимальное кол-во повторов, также контроль дисперсии
             double numForPower = Math.Pow(maxCountForMember, 1 / membersCount); //основание степени
             List<double> powers = new List<double>();
 
@@ -24,8 +25,16 @@ namespace MultiplicativeModel
 
         public static void ShowResult(List<double> values)
         {
-            for (int i = 1; i < values.Count+1; i++)
-                Console.WriteLine($"{i}: {Math.Round(values[i-1])}");
+            for (int i = 1; i < values.Count + 1; i++)
+                Console.WriteLine($"{i}: {Math.Round(values[i - 1])}");
+
+            double countSum = values.Sum();
+
+            double lastItemInPercent = values.Last() / values.Sum() * 100;
+
+            Console.WriteLine();
+            Console.WriteLine("Last item in percent: " + lastItemInPercent);
+            Console.WriteLine();
 
             Console.WriteLine("Avverage: " + GetAvverage(values));
             Console.WriteLine("Disperion: " + GetDisperion(values));
@@ -33,9 +42,14 @@ namespace MultiplicativeModel
 
             Console.WriteLine();
 
-            double countSum = values.Sum();
 
             Console.WriteLine("queue size: " + countSum);
+
+            double percent80 = values.Count * 0.80;
+            var last20percentValues = values.Skip((int)Math.Round(percent80)).ToArray();
+            double last20percentSum = last20percentValues.Sum();
+            double last20percentSumInPercent = last20percentSum / values.Sum() * 100;
+            Console.WriteLine($"Last 20 percent sum: {last20percentSum} ({last20percentSumInPercent} %)");
         }
 
         public static double GetAvverage(List<double> values)
